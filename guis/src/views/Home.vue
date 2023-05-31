08b42caccfddb57eb89c14b4fd0f729ae772f823
Rohan <71481034+rohangardiner@users.noreply.github.com>
2023-05-31 15:15:49 +1000

    added GitHub link

--- guis/src/views/Home.vue
+++ guis/src/views/Home.vue
@@ -4,4 +4,5 @@
     7GUIs (7 Graphical User Interfaces) is a programming benchmark to compare implementations in terms of their notation. 7GUIs defines 7 tasks that represent typical challenges in GUI programming.<br>
     <a href="https://eugenkiss.github.io/7guis/tasks" target="_blank">More info</a>
   </p>
+  <p><a href="https://github.com/rohangardiner">GitHub/rohangardiner</a></p>
 </template>
